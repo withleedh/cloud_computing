542e3d9c2fc34e6e65e0735348fc1b773a9d07e3
Dongho <dongho.lee@kakao.com>
2022-10-06 23:00:47 +0900

    fix database.py

--- shop2/database.py
+++ shop2/database.py
@@ -1,4 +1,6 @@
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import json
@@ -6,8 +8,15 @@ from pathlib import Path
 
 json_base_path = Path(__file__).resolve().parent
 db_config_path = json.load(open(json_base_path / "database.json"))
+sql_driver = 'ODBC+Driver+17+for+SQL+Server'
+sql_server = db_config_path["host"]
+sql_database = db_config_path["database"]
+sql_username = db_config_path["user"]
+sql_password = db_config_path["password"]
 
-engine = create_engine('mssql+pyodbc://{user}:{password}@{host}/{database}?driver=ODBC+Driver+17+for+SQL+Server'.format(**db_config_path), convert_unicode=True, echo=True)
+connection_string = f"DRIVER={sql_driver};SERVER={sql_server};DATABASE={sql_database};UID={sql_username};PWD={sql_password}"
+connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string,})
+engine = sqlalchemy.create_engine(connection_url,convert_unicode=True, echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
